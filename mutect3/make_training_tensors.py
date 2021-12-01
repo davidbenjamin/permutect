c44a9c874ad8f05d86c15ee13f280235b08bf89f
davidben <davidben@broadinstitute.org>
2021-12-01 12:09:15 -0500

    perhaps successfully made training pickle from VCF

--- mutect3/make_training_tensors.py
+++ mutect3/make_training_tensors.py
@@ -4,14 +4,14 @@ from mutect3 import tensors
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--table', help='table of GATK output', required=True)
+    parser.add_argument('--input', help='VCF from GATK', required=True)
     parser.add_argument('--tumor', help='tumor sample name', required=True)
     parser.add_argument('--normal', help='normal sample name')
     parser.add_argument('--output', help='output path for pickled list of tensors', required=True)
     args = parser.parse_args()
 
     # if --normal not specified, args.normal is None, which works all the way through
-    data = tensors.make_tensors(args.table, is_training=True, sample_name=args.tumor, normal_sample_name=args.normal)
+    data = tensors.make_training_tensors_from_vcf(args.input, args.tumor, args.normal)
     tensors.make_pickle(args.output, data)
 
 
--- mutect3/tensors.py
+++ mutect3/tensors.py
@@ -303,154 +303,13 @@ TLOD_THRESHOLD = 6  # we are classified artifacts other than sequencing errors d
 NON_ARTIFACT_PER_ARTIFACT = 20  # ratio of non-artifact to artifact in unsupervised training data
 
 
-class TableReader:
-    # separator between different alleles in the FRS (featurized read set) INFO field from Mutect output
-    ALLELE_SEPARATOR = '|'
-
-    def __init__(self, header_tokens, tumor_sample, normal_sample=None):
-        self.site_info_indices = self._get_indices(header_tokens, ["CHROM", "POS", "REF", "ALT", "POPAF"])
-        self.mutect_indices = self._get_indices(header_tokens, ["FILTER", "TLOD", tumor_sample + ".DP"])
-        self.variant_info_indices = self._get_indices(header_tokens, ["HEC", "HAPCOMP", "HAPDOM", "REF_BASES"])
-        self.status_idx = TableReader._index_if_exists(header_tokens, "STATUS")
-        self.tumor_idx = TableReader._index_if_exists(header_tokens, tumor_sample + ".FRS")
-
-        # optional normal data
-        if normal_sample is not None:
-            self.normal_idx, self.normal_dp_idx = self._get_indices(header_tokens,
-                                                                    [normal_sample + ".FRS", normal_sample + ".DP"])
-
-    def variant_info(self, tokens) -> VariantInfo:
-        hec, hapcomp, hapdom, ref_bases = (tokens[idx] for idx in self.variant_info_indices)
-        return VariantInfo([int(n) for n in hec.split(",")], int(hapcomp), float(hapdom), ref_bases)
-
-    def site_info(self, tokens) -> SiteInfo:
-        chrom, pos, ref, alt, popaf = (tokens[idx] for idx in self.site_info_indices)
-        return SiteInfo(chrom, int(pos), ref, alt, float(popaf))
-
-    def mutect_info(self, tokens) -> MutectInfo:
-        filters, tlod, tumor_dp = (tokens[idx] for idx in self.mutect_indices)
-        return MutectInfo(float(tlod), int(tumor_dp), set(filters.split(",")))
-
-    def status(self, tokens):
-        return tokens[self.status_idx]
-
-    def tumor_ref_and_alt(self, tokens, ref_downsample=None):
-        return TableReader._get_read_tensors(tokens[self.tumor_idx], ref_downsample)
-
-    def normal_dp(self, tokens):
-        return int(tokens[self.normal_dp_idx])
-
-    def normal_ref_and_alt(self, tokens, ref_downsample=None):
-        return TableReader._get_read_tensors(tokens[self.normal_idx], ref_downsample)
-
-    @staticmethod
-    def _index_if_exists(lis, element):
-        return lis.index(element) if element in lis else None
-
-    @staticmethod
-    def _get_indices(lis, elements):
-        return tuple(TableReader._index_if_exists(lis, element) for element in elements)
-
-    # get the ref and alt tensors from the VariantsToTable raw output, convert to torch tensors, downsample ref if needed
-    @staticmethod
-    def _get_read_tensors(token, ref_downsample=None):
-        tokens = token.split(TableReader.ALLELE_SEPARATOR)
-        ref, alt = tuple(np.fromstring(x, dtype=int, sep=',').reshape((-1, NUM_READ_FEATURES)) for x in tokens)
-        ref, alt = torch.from_numpy(ref).float(), torch.from_numpy(alt).float()
-        return downsample(ref, ref_downsample), alt
-
-
-# this takes a table from VariantsToTable and produces a Python list of Datum objects
-def make_tensors(raw_file, is_training, sample_name, normal_sample_name=None, shuffle=True) -> List[Datum]:
-    trusted_m2_filters = {"contamination", "germline", "weak_evidence"}
+def make_training_tensors_from_vcf(vcf, tumor, normal=None, shuffle=True) -> List[Datum]:
     data = []
 
     # simple method to balance data: for each k-alt-read artifact there are
     # NON_ARTIFACT_PER_ARTIFACT (downsampled) k-alt-read non-artifacts.
     unmatched_counts_by_type = defaultdict(list)
-
-    with open(raw_file) as fp:
-        reader = TableReader(fp.readline().split(), sample_name, normal_sample_name)
-        pbar = tqdm(enumerate(fp))
-        for n, line in pbar:
-
-            tokens = line.split()
-            site_info = reader.site_info(tokens)
-            m2_data = reader.mutect_info(tokens)
-            filters = m2_data.filters()
-            popaf = site_info.popaf()
-
-            # For testing we still rely on the M2 contamination, germline, and evidence filters.
-            # For training, contamination has low-AF but is not an artifact, which we don't want
-            if not is_training and filters.intersection(trusted_m2_filters):
-                continue
-            elif "contamination" in filters:
-                continue
-
-            ref_tensor, alt_tensor = reader.tumor_ref_and_alt(tokens, REF_DOWNSAMPLE)
-            alt_count = len(alt_tensor)
-            if alt_count == 0 or len(ref_tensor) < MIN_REF:
-                continue
-
-            tumor_af = alt_count / m2_data.tumor_depth()
-            is_artifact = None
-
-            has_normal = normal_sample_name is not None
-            normal_dp = reader.normal_dp(tokens) if has_normal else 0
-            normal_ref, normal_alt = reader.normal_ref_and_alt(tokens, REF_DOWNSAMPLE) if has_normal else (None, None)
-            normal_alt_count = len(normal_alt) if has_normal else 0
-            normal_af = 0 if normal_dp == 0 else normal_alt_count / normal_dp
-
-            likely_seq_error = "weak_evidence" in filters or m2_data.tlod() < TLOD_THRESHOLD
-            likely_germline = has_normal and normal_af > 0.2
-
-            # extremely strict criteria because there are so many germline variants we can afford to waste a lot
-            definite_germline = not likely_seq_error and popaf < COMMON_POPAF and (
-                    tumor_af > 0.35 and popaf < COMMON_POPAF) and (normal_af > 0.35 if has_normal else True)
-
-            if is_training:
-                unmatched_artifact_counts = unmatched_counts_by_type[
-                    utils.get_variant_type(site_info.alt(), site_info.ref())]
-
-                # low AF in tumor and normal, rare in population implies artifact
-                if not (likely_seq_error or likely_germline) and tumor_af < 0.2 and popaf > RARE_POPAF:
-                    unmatched_artifact_counts.extend([alt_count] * NON_ARTIFACT_PER_ARTIFACT)
-                    is_artifact = 1
-                # high AF in tumor and normal, common in population implies germline, which we downsample
-                elif definite_germline and unmatched_artifact_counts:
-                    downsample_count = min(alt_count, unmatched_artifact_counts.pop())
-                    alt_tensor = alt_tensor[torch.randperm(alt_count)[:downsample_count]]
-                    is_artifact = 0
-                # inconclusive -- unlabeled datum
-                # to avoid too large a dataset, we try to bias toward possible artifacts and keep out obvious sequencing errors
-                elif m2_data.tlod() > 4.0 and tumor_af < 0.3:
-                    is_artifact = None
-                    downsample_count = min(alt_count, 10)
-                    alt_tensor = alt_tensor[torch.randperm(alt_count)[:downsample_count]]
-                else:
-                    continue
-            else:
-                # use Concordance STATUS field for test labels
-                status = reader.status(tokens)
-                is_artifact = (status == "FP" or status == "FTN")
-
-            # assembly complexity site-level annotations
-            info_tensor = reader.variant_info(tokens).info_tensor()
-
-            data.append(Datum(ref_tensor, alt_tensor, info_tensor, site_info, m2_data, is_artifact, normal_dp,
-                              normal_alt_count))
-        if shuffle:
-            random.shuffle(data)
-        return data
-
-
-def make_training_tensors_from_vcf(vcf: pysam.VariantFile, tumor, normal=None, shuffle=True) -> List[Datum]:
-    data = []
-
-    # simple method to balance data: for each k-alt-read artifact there are
-    # NON_ARTIFACT_PER_ARTIFACT (downsampled) k-alt-read non-artifacts.
-    unmatched_counts_by_type = defaultdict(list)
-    for rec in vcf:
+    for rec in pysam.VariantFile(vcf):
         datum = unlabeled_datum_from_vcf(rec, tumor, normal, REF_DOWNSAMPLE)
         alt_count = len(datum.alt_tensor())
         if alt_count == 0 or len(datum.ref_tensor()) < MIN_REF:
@@ -471,7 +330,7 @@ def make_training_tensors_from_vcf(vcf: pysam.VariantFile, tumor, normal=None, s
                 tumor_af > 0.35 and popaf < COMMON_POPAF) and (normal_af > 0.35 if has_normal else True)
 
         unmatched_artifact_counts = unmatched_counts_by_type[
-            utils.get_variant_type(datum.site_info.alt(), datum.site_info.ref())]
+            utils.get_variant_type(datum.site_info().alt(), datum.site_info().ref())]
 
         # low AF in tumor and normal, rare in population implies artifact
         if not (likely_seq_error or likely_germline) and tumor_af < 0.2 and popaf > RARE_POPAF:
@@ -494,34 +353,3 @@ def make_training_tensors_from_vcf(vcf: pysam.VariantFile, tumor, normal=None, s
 
         data.append(datum)
     return data
-
-
-def generate_pickles(tumor_table, normal_table, tumor_sample, normal_sample, pickle_dir, pickle_prefix):
-    pair_train_pickle, small_pair_train_pickle, tumor_train_pickle, normal_train_pickle, test_pickle, small_test_pickle = \
-        (pickle_dir + pickle_prefix + suffix for suffix in
-         ('-pair-train.pickle', '-small-pair-train.pickle', '-tumor-train.pickle', \
-          '-normal-train.pickle', '-test.pickle', '-small-test.pickle'))
-
-    # we form a few kinds of training data: tumor data using the normal
-    # (the normal doesn't change the format but helps make better truth guesses)
-    print("Generating and pickling tumor tensors for training using tumor and normal")
-    pair_train_data = make_tensors(tumor_table, True, tumor_sample, normal_sample)
-    make_pickle(pair_train_pickle, pair_train_data)
-
-    print("Generating and pickling small (by 10x) tumor tensors for training using tumor and normal")
-    make_pickle(small_pair_train_pickle, pair_train_data[:int(len(pair_train_data) / 10)])
-
-    print("Generating and pickling tumor tensors for training using only tumor")
-    tumor_train_data = make_tensors(tumor_table, True, tumor_sample)
-    make_pickle(tumor_train_pickle, tumor_train_data)
-
-    print("Generating and pickling normal tensors for training using only normal")
-    normal_train_data = make_tensors(normal_table, True, normal_sample)
-    make_pickle(normal_train_pickle, normal_train_data)
-
-    print("Generating and pickling tumor tensors for testing using STATUS labels")
-    test_data = make_tensors(tumor_table, False, tumor_sample, normal_sample)
-    make_pickle(test_pickle, test_data)
-
-    print("Generating and pickling small (by 10x) tumor tensors for for testing using STATUS labels")
-    make_pickle(small_test_pickle, test_data[:int(len(test_data) / 10)])
