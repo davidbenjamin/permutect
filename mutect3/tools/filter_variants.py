5cf9ce17e493f56d2bf9cc3a66d2af1ef4de1994
David Benjamin <davidben@broadinstitute.org>
2022-09-05 11:37:55 -0400

    more debug

--- mutect3/tools/filter_variants.py
+++ mutect3/tools/filter_variants.py
@@ -94,7 +94,9 @@ def get_segmentation(segments_file) -> defaultdict:
 
 
 def main():
+    print("DEBUG 3")
     args = parse_arguments()
+    print("DEBUG 4")
 
     make_filtered_vcf(saved_artifact_model=getattr(args, constants.M3_MODEL_NAME),
                       initial_log_variant_prior=getattr(args, constants.INITIAL_LOG_VARIANT_PRIOR_NAME),
