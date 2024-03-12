2d0ab5a1c0fc6118f604c39b2196d1d442d1c50e
David Benjamin <davidben@broadinstitute.org>
2024-03-12 17:34:01 -0400

    some debug prints

--- mutect3/data/read_set.py
+++ mutect3/data/read_set.py
@@ -201,6 +201,8 @@ class ReadSetBatch:
         list_of_ref_tensors = [item.ref_reads_2d for item in data] if self.ref_count > 0 else []
         self.reads_2d = torch.from_numpy(np.vstack(list_of_ref_tensors + [item.alt_reads_2d for item in data])).float()
 
+        print(data[0].alt_extra_tensor_3d)
+        print(data[0].alt_extra_tensor_3d.to_dense())
         debug = data[0].alt_extra_tensor_3d.to_dense()
         #list_of_ref_extra_tensors = [item.ref_extra_tensor_3d for item in data] if self.ref_count > 0 else []
         #list_of_alt_extra_tensors = [item.alt_extra_tensor_3d for item in data]
