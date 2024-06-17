22e2e22bbc93e1076406cbaaa7e53d71be1290ff
David Benjamin <davidben@broadinstitute.org>
2024-06-17 18:38:10 -0400

    upgrade to newest mmap_ninja (#119)

--- mmap_ninja/ragged.py
+++ mmap_ninja/ragged.py
@@ -1,16 +1,17 @@
+from copy import copy
 from pathlib import Path
 from typing import Union, Sequence
 
 import numpy as np
 
-from mmap_ninja import numpy, base
+from mmap_ninja import base, numpy
 
 
-def np_shape_extend(shapes, arr):
+def _np_shape_extend(shapes, arr):
     numpy.extend(shapes, arr)
 
 
-def ragged_shape_extend(shapes, arr):
+def _ragged_shape_extend(shapes, arr):
     shapes.extend(arr)
 
 
@@ -18,12 +19,13 @@ class RaggedMmap:
     def __init__(
         self,
         out_dir: Union[str, Path],
-        mode="r",
         wrapper_fn=None,
+        mode="r",
         starts_key="starts",
         ends_key="ends",
         shapes_key="shapes",
         flattened_shapes_key="flattened_shapes",
+        copy_before_wrapper_fn=True,
     ):
         out_dir = Path(out_dir)
         out_dir.mkdir(exist_ok=True)
@@ -39,10 +41,27 @@ class RaggedMmap:
         self.out_dir = out_dir
         self.wrapper_fn = wrapper_fn
         self.mode = mode
-        self.shapes_are_flat = bool(base.file_to_int(self.out_dir / "shapes_are_flat.ninja"))
 
+        self.shapes_are_flat = None
+        self.shapes_ctor = None
+        self.memmap = None
+        self.starts = None
+        self.ends = None
+        self.shapes = None
+        self.flattened_shapes = None
+        self.range = None
+        self.n = None
+        self.range = None
+        self.shapes_extension_fn = None
+
+        if (self.out_dir / "shapes_are_flat.ninja").exists():
+            self._reload_fields()
+
+        self.copy_before_wrapper_fn = copy_before_wrapper_fn
+
+    def _reload_fields(self):
+        self.shapes_are_flat = bool(base._file_to_int(self.out_dir / "shapes_are_flat.ninja"))
         self.shapes_ctor = numpy.open_existing if self.shapes_are_flat else RaggedMmap
-
         self.memmap = numpy.open_existing(self.out_dir, mode=self.mode)
         self.starts = numpy.open_existing(self.out_dir / self.starts_key, mode=self.mode)
         self.ends = numpy.open_existing(self.out_dir / self.ends_key, mode=self.mode)
@@ -51,7 +70,7 @@ class RaggedMmap:
         self.range = np.arange(len(self.starts), dtype=np.int64)
         self.n = len(self.shapes)
         self.range = np.arange(self.n)
-        self.shapes_extension_fn = np_shape_extend if self.shapes_are_flat else ragged_shape_extend
+        self.shapes_extension_fn = _np_shape_extend if self.shapes_are_flat else _ragged_shape_extend
 
     def get_multiple(self, item):
         indices = self.range[item]
@@ -68,6 +87,8 @@ class RaggedMmap:
         self.memmap[start:end] = value
 
     def __getitem__(self, item):
+        if self.starts is None:
+            raise IndexError(f"RaggedMmap is empty!")
         if np.isscalar(item):
             return self.get_single(item)
         return self.get_multiple(item)
@@ -78,6 +99,8 @@ class RaggedMmap:
         return self.set_multiple(key, value)
 
     def __len__(self):
+        if self.starts is None:
+            return 0
         return len(self.starts)
 
     def get_single(self, item):
@@ -86,10 +109,13 @@ class RaggedMmap:
         shape = self.shapes[item]
         res = self.memmap[start:end]
         if self.shapes_are_flat and shape[0] == 0:
-            res = res.item()
+            if len(res) == 1:
+                res = res.item()
         else:
             res = res.reshape(shape)
         if self.wrapper_fn is not None:
+            if self.copy_before_wrapper_fn:
+                res = copy(res)
             res = self.wrapper_fn(res)
         return res
 
@@ -97,22 +123,22 @@ class RaggedMmap:
         self.extend([array])
 
     def extend(self, arrays: Sequence[np.ndarray]):
-        numpy_bytes_slices = numpy.lists_of_ndarrays_to_bytes(arrays, self.memmap.dtype)
+        if self.starts is None:
+            RaggedMmap.from_lists(self.out_dir, arrays)
+            self._reload_fields()
+            return
+        numpy_bytes_slices = numpy._lists_of_ndarrays_to_bytes(arrays, self.memmap.dtype)
         numpy.extend(self.memmap, numpy_bytes_slices.buffer)
         end = self.ends[-1]
         numpy.extend(self.starts, end + numpy_bytes_slices.starts)
         numpy.extend(self.ends, end + numpy_bytes_slices.ends)
         numpy.extend(self.flattened_shapes, numpy_bytes_slices.flattened_shapes)
         self.shapes_extension_fn(self.shapes, numpy_bytes_slices.shapes)
+        self._reload_fields()
 
-        self.memmap = numpy.open_existing(self.out_dir, mode=self.mode)
-        self.starts = numpy.open_existing(self.out_dir / self.starts_key, mode=self.mode)
-        self.ends = numpy.open_existing(self.out_dir / self.ends_key, mode=self.mode)
-        self.shapes = self.shapes_ctor(self.out_dir / self.shapes_key, mode=self.mode)
-        self.flattened_shapes = numpy.open_existing(self.out_dir / self.flattened_shapes_key, mode=self.mode)
-        self.range = np.arange(len(self.starts), dtype=np.int64)
-        self.n = len(self.shapes)
-        self.range = np.arange(self.n)
+    def __repr__(self):
+        base_repr = super().__repr__()
+        return f"{base_repr} of length: {len(self)}"
 
     @classmethod
     def from_lists(
@@ -129,11 +155,12 @@ class RaggedMmap:
     ):
         out_dir = Path(out_dir)
         out_dir.mkdir(exist_ok=True)
-        numpy_bytes_slices = numpy.lists_of_ndarrays_to_bytes(lists, dtype)
+        numpy_bytes_slices = numpy._lists_of_ndarrays_to_bytes(lists, dtype)
         numpy.from_ndarray(out_dir / starts_key, np.array(numpy_bytes_slices.starts, dtype=np.int64))
         numpy.from_ndarray(out_dir / ends_key, np.array(numpy_bytes_slices.ends, dtype=np.int64))
         shapes_are_flat = all([len(shape) == 1 for shape in numpy_bytes_slices.shapes])
-        base.int_to_file(int(shapes_are_flat), out_dir / "shapes_are_flat.ninja")
+        base._int_to_file(int(shapes_are_flat), out_dir / "shapes_are_flat.ninja")
+
         if shapes_are_flat:
             numpy.from_ndarray(out_dir / shapes_key, numpy_bytes_slices.shapes)
         else:
@@ -142,6 +169,7 @@ class RaggedMmap:
             out_dir / flattened_shapes_key, np.array(numpy_bytes_slices.flattened_shapes, dtype=np.int64)
         )
         numpy.from_ndarray(out_dir, np.array(numpy_bytes_slices.buffer))
+        base._str_to_file("ragged", out_dir / "type.ninja")
         return cls(
             out_dir=out_dir,
             wrapper_fn=wrapper_fn,
@@ -160,5 +188,5 @@ class RaggedMmap:
             batch_size=batch_size,
             verbose=verbose,
             batch_ctor=cls.from_lists,
-            **kwargs
+            **kwargs,
         )
