6b5edb870db6c04abde3a6ed227e89d5e3f2beba
David Benjamin <davidben@broadinstitute.org>
2025-02-09 11:49:15 -0500

    Delete some unused, obsolete WDLs and tools (#178)

--- setup.py
+++ setup.py
@@ -14,9 +14,7 @@ setup(
                             'filter_variants=permutect.tools.filter_variants:main',
                             'preprocess_dataset=permutect.tools.preprocess_dataset:main',
                             'edit_dataset=permutect.tools.edit_dataset:main',
-                            'prune_dataset=permutect.tools.prune_dataset:main',
-                            'evaluate_model=permutect.tools.evaluate_model:main',
-                            'compare_to_mutect2=permutect.tools.compare_to_mutect2:main'
+                            'prune_dataset=permutect.tools.prune_dataset:main'
                             ]
     }
 )
