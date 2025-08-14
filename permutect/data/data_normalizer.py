3722c7038a0091289b588b4a1e5ebf32c0312992
David Benjamin <davidben@broadinstitute.org>
2025-08-13 20:48:01 -0400

    bug fix and hard-coding map qual and base qual logic

--- permutect/data/data_normalizer.py
+++ permutect/data/data_normalizer.py
@@ -1,5 +1,11 @@
 import torch
 
+MAPPING_QUALITY_COLUMN_INDEX = 0
+BASE_QUALITY_COLUMN_INDEX = 1
+MAX_MAP_QUALITY = 60
+MAX_BASE_QUALITY = 30
+DEFAULT_CAP = 1e4
+
 
 class DataNormalizer:
     '''
@@ -7,7 +13,7 @@ class DataNormalizer:
     The normalization is subtract the first element and divide by the second.
     '''
     def __init__(self):
-        self.shift_and_scale_by_column = {}
+        self.cap_shift_scale_by_column = {}
 
     def fit(self, array2d):
         tensor2d = torch.from_numpy(array2d)
@@ -22,17 +28,21 @@ class DataNormalizer:
             #    in this case identify the max MQ as some high percentile and divide by this max MQ
             # 3) majority is zero -- do nothing
             # 4) otherwise, median and IQR
-            if torch.mean(torch.logical_or(column == 0, column == 1).float()) > 0.999999:     # binary column
-                self.shift_and_scale_by_column[col_idx] = (0, 1)    # identity transform
+            if col_idx == MAPPING_QUALITY_COLUMN_INDEX:    # THIS IS MAPPING QUALITY!!! hard-code to cap at 60 and scale by 60
+                self.cap_shift_scale_by_column[col_idx] = (MAX_MAP_QUALITY, 0, MAX_MAP_QUALITY)
+            elif col_idx == BASE_QUALITY_COLUMN_INDEX:    # THIS IS BASE QUALITY!!! hard-code to cap at 30 and scale by 30
+                self.cap_shift_scale_by_column[col_idx] = (MAX_BASE_QUALITY, 0, MAX_BASE_QUALITY)
+            elif torch.mean(torch.logical_or(column == 0, column == 1).float()) > 0.999999:     # binary column
+                self.cap_shift_scale_by_column[col_idx] = (DEFAULT_CAP, 0, 1)    # identity transform
             elif torch.mean(torch.logical_and(column > -1, column < 61).float()) > 0.99 and torch.median(float_column) > 40:
                 num_mq_columns += 1
                 max_mq = torch.quantile(float_column, q=0.99).item()
-                self.shift_and_scale_by_column[col_idx] = (0, max_mq)
+                self.cap_shift_scale_by_column[col_idx] = (DEFAULT_CAP, 0, max_mq)
             elif torch.mean((column == 0).float()) > 0.6:     # mainly-zero
-                self.shift_and_scale_by_column[col_idx] = (0, 1)    # identity transform
+                self.cap_shift_scale_by_column[col_idx] = (DEFAULT_CAP, 0, 1)    # identity transform
             else:
                 iqr = (torch.quantile(float_column, q=0.75) - torch.quantile(float_column, q=0.25)).item()
-                self.shift_and_scale_by_column[col_idx] = (torch.median(float_column).item(), iqr)
+                self.cap_shift_scale_by_column[col_idx] = (DEFAULT_CAP, torch.median(float_column).item(), iqr)
 
         # TODO: turn this check on if reads, but not for INFO of course
         # assert num_mq_columns == 1, f"Did not find exactly one MQ column, found {num_mq_columns}."
@@ -40,7 +50,7 @@ class DataNormalizer:
     def transform(self, array2d):
         result = torch.from_numpy(array2d)
         for col_idx in range(result.shape[1]):
-            shift, scale = self.shift_and_scale_by_column[col_idx]
-            result[:, col_idx] = (result[:, col_idx] - shift) / scale
+            cap, shift, scale = self.cap_shift_scale_by_column[col_idx]
+            result[:, col_idx] = (torch.clamp(result[:, col_idx], max=cap) - shift) / scale
         return result.numpy()
 
