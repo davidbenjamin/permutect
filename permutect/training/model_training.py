e78dd5f57773512a7c91b05d3c322db04fbc825a
David Benjamin <davidben@broadinstitute.org>
2025-09-02 16:21:13 -0400

    Euclidean 7 (misnomer): no rigid transform -- just adapt all feature clustering params, nothing else

--- permutect/training/model_training.py
+++ permutect/training/model_training.py
@@ -85,9 +85,9 @@ def train_artifact_model(model: ArtifactModel, dataset: ReadsDataset, training_p
             # in calibration epoch, freeze the model except for calibration
             if domain_adaptation and epoch_type == Epoch.TRAIN:
                 freeze(model.parameters())
-                unfreeze(model.set_pooling.parameters())
-                unfreeze(model.domain_adaptation_parameters())
-                unfreeze(model.calibration_parameters())
+                #unfreeze(model.set_pooling.parameters())
+                #unfreeze(model.domain_adaptation_parameters())
+                #unfreeze(model.calibration_parameters())
                 unfreeze(model.feature_clustering.parameters())
             if is_calibration_epoch and epoch_type == Epoch.TRAIN:
                 freeze(model.parameters())
