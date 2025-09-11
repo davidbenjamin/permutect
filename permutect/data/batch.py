9332ed5064cc180086dc3a0982451ec240007144
David Benjamin <davidben@broadinstitute.org>
2025-09-11 17:33:40 -0400

    fixing some accompanying changes in Batch class, maybe a few more remain

--- permutect/data/batch.py
+++ permutect/data/batch.py
@@ -10,7 +10,7 @@ import numpy as np
 from permutect.data.count_binning import ref_count_bin_name, NUM_REF_COUNT_BINS, alt_count_bin_name, NUM_ALT_COUNT_BINS, \
     logit_bin_name, NUM_LOGIT_BINS, ref_count_bin_indices, alt_count_bin_indices, logit_bin_indices, \
     ref_count_bin_index, alt_count_bin_index
-from permutect.data.datum import Datum
+from permutect.data.datum import Datum, FLOAT_TO_LONG_MULTIPLIER
 from permutect.misc_utils import gpu_if_available
 from permutect.utils.array_utils import flattened_indices
 from permutect.utils.enums import Label, Variation
@@ -78,15 +78,15 @@ class Batch:
         return self.data[:, Datum.ORIGINAL_NORMAL_DEPTH_IDX]
 
     def get_info_be(self) -> Tensor:
-        return self.data[:, self.info_start:self.info_end] / Datum.FLOAT_TO_LONG_MULTIPLIER
+        return self.data[:, self.info_start:self.info_end] / FLOAT_TO_LONG_MULTIPLIER
 
     # this is -log10ToLog(TLOD) - log(tumorDepth + 1);
     def get_seq_error_log_lks(self) -> Tensor:
-        return self.data[:, Datum.SEQ_ERROR_LOG_LK_IDX] / Datum.FLOAT_TO_LONG_MULTIPLIER
+        return self.data[:, Datum.SEQ_ERROR_LOG_LK_IDX] / FLOAT_TO_LONG_MULTIPLIER
 
     # this is -log10ToLog(NALOD) - log(normalDepth + 1)
     def get_normal_seq_error_log_lks(self) -> Tensor:
-        return self.data[:, Datum.NORMAL_SEQ_ERROR_LOG_LK_IDX] / Datum.FLOAT_TO_LONG_MULTIPLIER
+        return self.data[:, Datum.NORMAL_SEQ_ERROR_LOG_LK_IDX] / FLOAT_TO_LONG_MULTIPLIER
 
     def get_haplotypes_bs(self) -> IntTensor:
         # each row is 1D array of integer array reference and alt haplotypes concatenated -- A, C, G, T, deletion = 0, 1, 2, 3, 4
