fd502539580a51e163601a1de4bb100c71b8fa94
David Benjamin <davidben@broadinstitute.org>
2025-11-15 22:46:03 -0500

    get rid of prefetching in prefetch generator

--- permutect/data/prefetch_generator.py
+++ permutect/data/prefetch_generator.py
@@ -1,6 +1,7 @@
 from torch.utils.data import DataLoader
 
 from permutect.data.datum import DEFAULT_GPU_FLOAT, DEFAULT_CPU_FLOAT
+from permutect.data.reads_batch import ReadsBatch
 from permutect.misc_utils import gpu_if_available
 
 
@@ -10,9 +11,16 @@ def prefetch_generator(dataloader: DataLoader, device=gpu_if_available()):
     dataloader must yield betches that have a copy_to method
     """
     print("Entering prefetch generator...")
-    loader_iter = iter(dataloader)
     is_cuda = device.type == 'cuda'
     dtype = DEFAULT_GPU_FLOAT if is_cuda else DEFAULT_CPU_FLOAT
+    batch_cpu: ReadsBatch
+    for batch_cpu in dataloader:
+        batch_gpu = batch_cpu.copy_to(device, dtype=dtype)
+        yield batch_gpu
+
+
+    """
+    loader_iter = iter(dataloader)
     print("about to get the first batch for the prefetch generator...")
     next_batch_cpu = next(loader_iter, None)
     next_batch = None if next_batch_cpu is None else next_batch_cpu.copy_to(device=device, dtype=dtype)
@@ -25,3 +33,4 @@ def prefetch_generator(dataloader: DataLoader, device=gpu_if_available()):
         next_batch_cpu = next(loader_iter, None)
         next_batch = None if next_batch_cpu is None else next_batch_cpu.copy_to(device, dtype=dtype)
         yield batch
+    """
