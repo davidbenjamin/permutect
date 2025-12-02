349c6f13eadcdac7e1a50a1f57888f353414c8ff
David Benjamin <davidben@broadinstitute.org>
2025-12-02 10:20:25 -0500

    using test file names

--- permutect/test/tools/test_preprocess_dataset.py
+++ permutect/test/tools/test_preprocess_dataset.py
@@ -5,14 +5,13 @@ from permutect.data.memory_mapped_data import MemoryMappedData
 from permutect.data.reads_dataset import ReadsDataset
 from permutect.tools import preprocess_dataset
 from permutect import constants
+from permutect.test.test_file_names import *
 
-OVERWRITE_SAVED_TARFILE = True
-
+OVERWRITE_SAVED_TARFILE = False
 
 def test_on_10_megabases_singular():
-    training_datasets = ["/Users/davidben/permutect/integration-tests/hiseqx-NA12878-80000-data.dataset"]
-    training_data_tarfile = tempfile.NamedTemporaryFile() if not OVERWRITE_SAVED_TARFILE else \
-        "/Users/davidben/permutect/integration-tests/preprocessed-dataset.tar"
+    training_datasets = [SMALL_PLAIN_TEXT_DATA]
+    training_data_tarfile = tempfile.NamedTemporaryFile() if not OVERWRITE_SAVED_TARFILE else PREPROCESSED_DATA
 
     tarfile_name = training_data_tarfile.name if not OVERWRITE_SAVED_TARFILE else training_data_tarfile
 
