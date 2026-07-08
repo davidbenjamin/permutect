34f5291070482e693804621111c395b636af4bd5
David Benjamin <davidben@broadinstitute.org>
2026-07-07 21:33:18 -0400

    updated filtering integration test

--- permutect/test/tools/test_filter_variants.py
+++ permutect/test/tools/test_filter_variants.py
@@ -21,8 +21,6 @@ def test_filtering_on_dream1_chr20():
     setattr(filtering_args, constants.TRAINABLE_PARAMETERS_NAME, ["CLUSTERING"])
     setattr(filtering_args, constants.OUTPUT_NAME, permutect_vcf.name)
     setattr(filtering_args, constants.TENSORBOARD_DIR_NAME, tensorboard_dir.name)
-    setattr(filtering_args, constants.BATCH_SIZE_NAME, 64)
-    setattr(filtering_args, constants.NUM_WORKERS_NAME, 0)
     setattr(filtering_args, constants.NUM_SPECTRUM_ITERATIONS_NAME, 2)
     setattr(filtering_args, constants.RECALL_WEIGHT_NAME, 1.0)
     setattr(filtering_args, constants.HET_BETA_NAME, 10)
@@ -35,5 +33,12 @@ def test_filtering_on_dream1_chr20():
     setattr(filtering_args, constants.NORMAL_MAF_SEGMENTS_NAME, None)
     setattr(filtering_args, constants.GERMLINE_MODE_NAME, False)
     setattr(filtering_args, constants.NO_GERMLINE_MODE_NAME, False)
+
+    # training hyperparameters
+    setattr(filtering_args, constants.BATCH_SIZE_NAME, 64)
+    setattr(filtering_args, constants.NUM_WORKERS_NAME, 2)
+    setattr(filtering_args, constants.NUM_EPOCHS_NAME, 2)
+    setattr(filtering_args, constants.LEARNING_RATE_NAME, 0.001)
+    setattr(filtering_args, constants.WEIGHT_DECAY_NAME, 0.01)
 
     filter_variants.main_without_parsing(filtering_args)
